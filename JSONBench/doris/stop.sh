b6deeafd68e91d077a934fa6ad60bff18295441a
Egor Malko <e.malko@bk.ru>
2026-02-21 22:48:50 +0300

    filter unwanted files

--- JSONBench/doris/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-${DORIS_FULL_NAME}/be/bin/stop_be.sh
-${DORIS_FULL_NAME}/fe/bin/stop_fe.sh
--- JSONBench/singlestore/uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# stop and remove all docker containers
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-
-sudo apt-get remove -y mysql-client
-sudo snap remove --purge docker
